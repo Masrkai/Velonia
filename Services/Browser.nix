8bbf71b7bec2c859dd5b8e23fd4f586505e194d2
Masrkai <118424417+Masrkai@users.noreply.github.com>
2026-07-06 09:47:59 +0300

    added zen browser properly

--- Services/Browser.nix
+++ Services/Browser.nix
@@ -7,17 +7,32 @@ let
   };
 
   zen-browser-pkgs = import zen-src { inherit pkgs; };
-in {
-  nixpkgs.config.zen.policies = {
-    ExtensionSettings = {
-      "uBlock0@raymondhill.net" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-        installation_mode = "force_installed";
+
+  zen-browser = pkgs.wrapFirefox zen-browser-pkgs.zen-browser-unwrapped {
+    pname = "zen-browser";
+    extraPolicies = {
+      ExtensionSettings = {
+        # https://addons.mozilla.org/firefox/downloads/latest/<EXTENSION-URL-NAME>/latest.xpi
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+        };
+
+        "gdpr@cavi.au.dk" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/consent-o-matic/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+         };
+
+         "{a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/user-agent-string-switcher/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+         };
       };
     };
   };
-
-  environment.systemPackages = [
-    zen-browser-pkgs.zen-browser
-  ];
+in {
+  environment.systemPackages = [ zen-browser ];
 }
--- Services/ztop.nix
+++ Services/ztop.nix
@@ -27,6 +27,8 @@
 
     ./SSI.nix
 
+    ./Browser.nix
+
 
   ];
 }
