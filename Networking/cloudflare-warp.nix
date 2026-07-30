61b081ea23ab64e04f64a215df6265dd63bad627
Masrkai <118424417+Masrkai@users.noreply.github.com>
2026-07-30 13:39:25 +0300

    silencing cloudflare warp

--- Networking/cloudflare-warp.nix
+++ Networking/cloudflare-warp.nix
@@ -7,6 +7,17 @@
     enable = true;
   };
 
+  # Override the systemd service environment to reduce verbosity
+  systemd.services.cloudflare-warp = {
+    environment = {
+      RUST_LOG = "warn";
+      RUST_BACKTRACE = "0";
+    };
+    serviceConfig = {
+      LogLevelMax = "warning";
+    };
+  };
+
   #? testing this you can run
   #> curl https://cloudflare.com/cdn-cgi/trace
 
