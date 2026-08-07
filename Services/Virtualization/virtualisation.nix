e65f44a9376e0f7fce1ee9512b46881cf27b7384
Masrkai <118424417+Masrkai@users.noreply.github.com>
2026-08-07 04:31:48 +0300

    experimenting with podman

--- Services/Virtualization/virtualisation.nix
+++ Services/Virtualization/virtualisation.nix
@@ -6,32 +6,43 @@ in
 
 {
 
-    users.groups =
-      lib.mkIf config.virtualisation.libvirtd.enable
-        (builtins.listToAttrs (map (g: {
-          name = g;
-          value.members = [ config.identity.username ];
-        }) virtualizationGroups));
+    # users.groups =
+    #   lib.mkIf config.virtualisation.libvirtd.enable
+    #     (builtins.listToAttrs (map (g: {
+    #       name = g;
+    #       value.members = [ config.identity.username ];
+    #     }) virtualizationGroups));
+
+
+users.users.${config.identity.username}.extraGroups = 
+  lib.mkIf config.virtualisation.libvirtd.enable
+    virtualizationGroups;
+
 
     # Rootless podman needs subuid/subgid ranges for your user.
     # (This is per-user; repeat for each account that runs containers.)
-    users.users.${config.identity.username}.subUidGidRange = 100;
+    # users.users.${config.identity.username}.subGidRange = 100;
+    # also to use this shell.nix on NixOS your user needs to be configured as such:
+  # users.extraUsers.adisbladis = {
+  #   subUidRanges = [{ startUid = 100000; count = 65536; }];
+  #   subGidRanges = [{ startGid = 100000; count = 65536; }];
+  # };
 
 
-    #--> Qemu KVM & VirtualBox
-    virtualisation = lib.mkForce {
+    hardware.nvidia-container-toolkit.enable = 
+    lib.elem "nvidia" config.services.xserver.videoDrivers;
 
+
+    virtualisation = {
 
       podman = {
-        enable = false;
+        enable = true;
         dockerCompat = true;
-        #defaultNetwork.settings.dns_enabled = true;
 
         # Required for `docker-compose` style networking to resolve names.
+        networkSocket.port = 2376;
+        networkSocket.openFirewall = false;
         defaultNetwork.settings.dns_enabled = true;
-
-        # Pull from Docker Hub / ghcr.io by default.
-        registries.search = [ "docker.io" "ghcr.io" "quay.io" ];
       };
 
       # oci-containers = {
@@ -78,9 +89,12 @@ in
       virglrenderer  # Required for 3D acceleration
 
       # Useful extras for CI-style work on the host:
+      podman
+      podman-tui # status of containers in the terminal
       podman-compose
       buildah    # for rebuilding images from Containerfiles
       skopeo     # copy/inspect images across registries
       dive       # inspect image layers
+
     ];
 }
